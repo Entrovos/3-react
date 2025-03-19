2dc2e4268b320adeae831a76861ab01df89424fd
pdmelo <104098283+pdmelo@users.noreply.github.com>
2025-03-18 21:35:46 -0400

    adding files

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="container">
+			<article>
+				<hgroup>
+					<div>
+						<h1>Hi friend ! </h1>
+					</div>
+				</hgroup>
+			</article>
+		</div>
+	);
+}
+
+export default App;
