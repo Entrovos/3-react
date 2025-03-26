441272e7e768d8c046771fe6b0dee00ea37348be
Aristedes Patelis <158300078+Entrovos@users.noreply.github.com>
2025-03-26 14:59:09 +0000

    Completed exercise 3.2, began exercise 3.3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import MainHeader from "./components/MainHeader";
 import PokemonList from "./components/PokemonList";
+import FetchData from "./components/FetchData";
 
 /* class Welcome extends React.Component {
 	constructor(props) {
@@ -18,18 +19,52 @@ import PokemonList from "./components/PokemonList";
 } */
 
 function App() {
-	const myPokemon = [
+	const [myPokemon, setMyPokemons] = useState([
 		"Pikachu",
 		"Arceus",
 		"Shedinja",
 		"Cubone",
 		"Chestnaught",
-	];
+	]);
 
 	const [count, setCount] = useState(0);
+	const [name, setName] = useState("Charmander");
+	const [newPokemon, setNewPokemon] = useState("");
+	const [bgColor, setBgColor] = useState("white");
+
+	const changeName = (e) => {
+		setName(e.target.value);
+	};
+
+	const addPokemon = (pokemonName) => {
+		setMyPokemons([...myPokemon, pokemonName]);
+	};
+
+	const setRandomBackgroundColor = () => {
+		const colors = [
+			"red",
+			"blue",
+			"green",
+			"yellow",
+			"purple",
+			"black",
+			"white",
+			"pink",
+			"orange",
+		];
+		const randomColor = colors[Math.floor(Math.random() * colors.length)];
+		setBgColor(randomColor);
+	};
+
+	useEffect(() => {
+		console.log("Component rendered!");
+	}, [count]);
 
 	return (
-		<div className="container">
+		<div
+			className="container"
+			style={{ backgroundColor: bgColor, padding: "100px" }}
+		>
 			<article>
 				<hgroup>
 					<div>
@@ -38,15 +73,40 @@ function App() {
 				</hgroup>
 
 				<p>Meet all my friends:</p>
-				<PokemonList pokemons={myPokemon} />
+
+				{/* <PokemonList pokemons={pokemon} /> */}
+				<FetchData />
 
 				<button onClick={() => alert("Hi there!")}>Click Me!</button>
 			</article>
-
 			<br />
+			<br />
+
 			<h3>My Counter: {count}</h3>
 			<button onClick={() => setCount(count + 1)}>Increase</button>
 			<button onClick={() => setCount(count - 1)}>Decrease</button>
+			<br />
+			<br />
+
+			<h1>Meet my Pokemon: {name}</h1>
+			<input type="text" value={name} onChange={changeName} />
+			<br />
+			<br />
+
+			<h1>Add a Pokemon</h1>
+			<input
+				type="text"
+				value={newPokemon}
+				onChange={(e) => setNewPokemon(e.target.value)}
+			/>
+			<button onClick={() => addPokemon(newPokemon)}>Add Pokemon</button>
+			<br />
+			<br />
+
+			<h1>Change Background Color</h1>
+			<button onClick={() => setRandomBackgroundColor()}>
+				Change Background
+			</button>
 		</div>
 	);
 }
--- client/src/components/PokemonList.jsx
+++ client/src/components/PokemonList.jsx
@@ -3,9 +3,18 @@ import React from "react";
 export default function PokemonList(props) {
 	return (
 		<ul>
-			{props.pokemons.map((pokemon, index) => (
-				<li key={index}>Welcome {pokemon}</li>
+			{props.pokemons.map((pokemon) => (
+				<li key={pokemon.id}>
+					{" "}
+					{pokemon.name} {pokemon.type}
+				</li>
 			))}
 		</ul>
+
+		// <ul>
+		// 	{props.pokemons.map((pokemon, index) => (
+		// 		<li key={index}>Welcome {pokemon}</li>
+		// 	))}
+		// </ul>
 	);
 }
