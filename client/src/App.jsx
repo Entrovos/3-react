c38dc0e71c130228989db8ac22a39ab6768e0518
Aristedes Patelis <158300078+Entrovos@users.noreply.github.com>
2025-03-27 08:59:54 -0400

    Revert "Completed exercise 3.1 and began exercise 3.2"
    This reverts commit bbc59908eba549d50f7a27893e75522cad119a87.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
-import MainHeader from "./components/MainHeader";
-import PokemonList from "./components/PokemonList";
-
-/* class Welcome extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<h1 style={{ color: "steelblue", fontWeight: "bold" }}>
-				Hi {this.props.name}!
-			</h1>
-		);
-	}
-} */
-
 function App() {
-	const myPokemon = [
-		"Pikachu",
-		"Arceus",
-		"Shedinja",
-		"Cubone",
-		"Chestnaught",
-	];
-
-	const [count, setCount] = useState(0);
-
 	return (
 		<div className="container">
 			<article>
 				<hgroup>
 					<div>
-						<MainHeader />{" "}
+						<h1>Hi friend ! </h1>
 					</div>
 				</hgroup>
-
-				<p>Meet all my friends:</p>
-				<PokemonList pokemons={myPokemon} />
-
-				<button onClick={() => alert("Hi there!")}>Click Me!</button>
 			</article>
-
-			<br />
-			<h3>My Counter: {count}</h3>
-			<button onClick={() => setCount(count + 1)}>Increase</button>
-			<button onClick={() => setCount(count - 1)}>Decrease</button>
 		</div>
 	);
 }
